2c5096998401260a6bbb3e095c8d9373e3d690bb
shinghuey27 <shinghuey27@hotmail.com>
2024-06-07 23:36:09 +0800

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Shing Huey on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
