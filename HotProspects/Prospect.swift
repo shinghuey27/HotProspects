684263cd48c44d58fb56f58d45eeab40e483329a
shinghuey27 <shinghuey27@hotmail.com>
2024-06-08 11:55:43 +0800

    done

---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,22 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Shing Huey on 08/06/2024.
+//
+
+import SwiftData
+
+@Model
+class Prospect {
+    var name: String
+    var emailAddress: String
+    var isContacted: Bool
+    
+    init(name: String, emailAddress: String, isContacted: Bool) {
+        self.name = name
+        self.emailAddress = emailAddress
+        self.isContacted = isContacted
+    }
+
+}
